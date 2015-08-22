e9ba1069b5b2446a7902304edd8eb6f3b8985fdb
Anthony Tellez <anthonygtellez@gmail.com>
2015-08-22 19:33:12 -0400

    Submitted to Splunkbase Ver 1.0.1
    This was the final version submitted to splunkbase

---
+++ pwned_command/bin/pwned.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2005-2011 Splunk Inc.  All Rights Reserved.  Version 4.x
+# Author: Nimish Doshi
+import sys,splunk.Intersplunk
+import string
+import urllib2
+
+emailfield="email"
+
+if len(sys.argv)>1 and len(sys.argv) != 4:
+    print "Usage |pwned email as <local-field> (or have email field name in data)"
+    sys.exit()
+elif len(sys.argv) == 4:
+    emailfield=sys.argv[3]
+
+results = []
+
+try:
+
+    results,dummyresults,settings = splunk.Intersplunk.getOrganizedResults()
+    
+    for r in results:
+        if "_raw" in r:
+            if emailfield in r:
+                try:
+                    eachuser=r[emailfield]
+                    response = urllib2.urlopen('https://haveibeenpwned.com/api/v2/breachedaccount/' + eachuser)
+                    data = response.read()
+                    response.close()
+                    r["isleaked"] = data
+                except:
+                    r["isleaked"] = ""
+
+except:
+    import traceback
+    stack =  traceback.format_exc()
+    results = splunk.Intersplunk.generateErrorResults("Error : Traceback: " + str(stack))
+
+splunk.Intersplunk.outputResults( results )
